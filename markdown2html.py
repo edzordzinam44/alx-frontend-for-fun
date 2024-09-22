f5b2354d2cda148ef28e26ad8427b82427c3eb31
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-09-22 08:31:35 +0000

    Refactor markdown2html.py to improve argument handling and heading conversion

--- markdown2html.py
+++ markdown2html.py
@@ -1,43 +1,27 @@
-#!/usr/bin/python3
-'''
-This is a script that codes markdown to HTML
-'''
+#!/usr/bin/env python3
 import sys
 import os
-import re
+import markdown
 
-if __name__ == '__main__':
 
-    # Test that the number of arguments passed is 2
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
 
-    # This store the arguments into variables
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
-    # Checks that the markdown file exists and is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f'Missing {input_file}', file=sys.stderr)
-        sys.exit(1)
+if not os.path.isfile(input_file):
+    print(f"Missing {input_file}", file=sys.stderr)
+    sys.exit(1)
 
-    with open(input_file, encoding='utf-8') as file_1:
-        html_content = []
-        md_content = [line[:-1] for line in file_1.readlines()]
-        for line in md_content:
-            heading = re.split(r'#{1,6} ', line)
-            if len(heading) > 1:
-                # Compute the number of the # present to
-                # determine heading level
-                h_level = len(line[:line.find(heading[1])-1])
-                # Append the html equivalent of the heading
-                html_content.append(
-                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
-                )
-            else:
-                html_content.append(line)
+with open(input_file, 'r', encoding='utf-8') as md_file:
+    markdown_content = md_file.read()
+    html_content = markdown.markdown(markdown_content)
 
-    with open(output_file, 'w', encoding='utf-8') as file_2:
-        file_2.writelines(html_content)
+
+with open(output_file, 'w', encoding='utf-8') as html_file:
+    html_file.write(html_content)
+
+
+sys.exit(0)
